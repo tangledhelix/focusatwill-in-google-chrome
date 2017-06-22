eb339cba143cdbc2593c73869e11bcd70f18042a
Dan Lowe <dan@tangledhelix.com>
2017-06-22 15:41:47 -0400

    Updates for new React-based player. Switch to JXA. Drop genres.
    Unfortunately this release drops support for switching genres. The new
    React-based controls make it difficult to do this automatically.
    
    Switched from Applescript to JavaScript (JXA).

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-This workflow will control the focusatwill.com tab in your Google Chrome browser. It will not bring Chrome to the front or activate the focus@will tab. If focusatwill.com is not already open, it will be opened.
+This workflow will control the focusatwill.com tab in your Google Chrome
+browser. It will not bring Chrome to the front or activate the focus@will tab.
 
-Hotkeys are stripped on import, so you'll need to go set what you prefer. You may assign hotkeys for the "play/pause" and "next track" actions.
+Hotkeys are stripped on import, so you'll need to go set what you prefer. You
+may assign hotkeys for the "play/pause" and "next track" actions.
 
-You can use the keyword "focus" (or "foc"). With no argument, play/pause is assumed.
+You can use the keyword "focus" (or "foc"). With no argument, play/pause is
+assumed.
 
     foc play    - play/pause
     foc pause   - play/pause
     foc p       - play/pause
     foc pp      - play/pause
-    
+
     foc next    - next track
     foc skip    - next track
     foc n       - next track
 
-You can also switch genres using the "focg" keyword. Unfortunately it does not seem to be possible to read data from a rendered web page into Alfred, so the list is fixed. If the site is updated, the list in this workflow will need manual updating.
-
-To change genres, use the "focg" keyword and start typing part of a genre name, such as "alph" for Alpha Chill.
-
-http://www.packal.org/workflow/playpause-focuswill-google-chrome
+Unfortunately, the new React-based player focus@will has implemented makes it
+difficult to switch genres. Without opening the genre list, the genre controls
+are not accessible. So for now, the genre selecting feature has been removed.
--- alfred_script.js
+++ alfred_script.js
@@ -1,65 +1,29 @@
-// Alpha-quality JSX port. Feature incomplete.
-// This is not used in the exported workflow yet.
-//
-// TODO: implement genre support
 
-var alfredQuery = "{query}";
-var alfredArgs = alfredQuery.split(" ");
+function run(argv) {
 
-var expectedUrl = "https://www.focusatwill.com/music/#player";
-var Chrome = Application("Google Chrome");
+  const appUrl = "https://www.focusatwill.com/app/music"
+  const browser = Application("Google Chrome")
 
-function run() {
+  let tab
 
-    var tab = findTab();
-    if (!tab) {
-        displayNotification();
-        return;
+  for (let w = 0; w < browser.windows.length; w++) {
+    for (let t = 0; t < browser.windows[w].tabs.length; t++) {
+      if (browser.windows[w].tabs[t].url().indexOf(appUrl) >= 0) {
+        tab = browser.windows[w].tabs[t]
+      }
     }
-    
-    if (["p", "pp", "play", "pause"].indexOf(alfredArgs[0]) >= 0) {
-        clickPlayPause(tab);
-    } else if (["n", "next", "skip"].indexOf(alfredArgs[0]) >= 0) {
-        clickNextTrack(tab);
-    } else if (["genre"].indexOf(alfredArgs[0]) >= 0) {
-        chooseGenre(alfredArgs[1]);
-    }
-}
+  }
 
-function findTab() {
-    var windowIndex, tabIndex, tabUrl;
-    
-    for (windowIndex = 0; windowIndex < Chrome.windows.length; windowIndex++) {
-        for (tabIndex = 0; tabIndex < Chrome.windows[windowIndex].tabs.length; tabIndex++) {
-            tabUrl = Chrome.windows[windowIndex].tabs[tabIndex].url();
-            if (tabUrl.indexOf(expectedUrl) >= 0) {
-                console.log("Found it - window " + windowIndex + ", tab " + tabIndex);
-                return Chrome.windows[windowIndex].tabs[tabIndex];
-            }
-        }
-    }
-    
-    return false;
-}
+  if (! tab) {
+    const me = Application.currentApplication()
+    me.includeStandardAdditions = true
+    me.displayNotification("Please open the site in Chrome.", {withTitle: "focus@will not found"})
+    return
+  }
 
-function displayNotification() {
-    var me = Application.currentApplication();
-    me.includeStandardAdditions = true;
-    me.displayNotification("Please open the site in Chrome.", {withTitle: "focus@will not found"});
-}
-
-function clickSomething(tab, targetClass) {
-    tab.execute({ javascript: "document.getElementsByClassName('" + targetClass + "')[0].click();" });
-}
-
-function clickPlayPause(tab) {
-    clickSomething(tab, "play");
-}
-
-function clickNextTrack(tab) {
-    clickSomething(tab, "next");
-}
-
-function chooseGenre(genre_idx) {
-    tab.execute({ javascript: "document.querySelectorAll('li.genre a')[" + genre_idx + "].click();" });
+  if (["p", "pp", "play", "pause"].indexOf(argv[0]) >= 0) {
+    tab.execute({ javascript: "document.querySelector('.playerControls .playButton').click();" })
+  } else if (["n", "next", "skip"].indexOf(argv[0]) >= 0) {
+    tab.execute({ javascript: "document.querySelector('.playerControls .skipButton').click();" })
+  }
 }
